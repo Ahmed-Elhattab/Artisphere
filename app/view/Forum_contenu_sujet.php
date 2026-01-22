e087054be24e2bee6a1fba08bcb0a4ff77a5f311
nareio <much.lio@laposte.net>
2026-01-22 10:02:38 +0100

    forum subject rectif

--- app/view/Forum_contenu_sujet.php
+++ app/view/Forum_contenu_sujet.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="styles_Thushjan.css">
-    <meta charset="UTF-8">
-    <title>Front</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link rel="stylesheet" href="header.css">
-    <title>Document</title>
-
-<body id="Forum_contenu_sujet">
-
-
 <!-- Contenue -->
 
-<div id="Forum">
+<div id="Forum" class="forum-topic-page">
+
     <a href="/artisphere/?controller=forum&action=index">Retour au forum</a>
     
     <div class="boite-sujet">
@@ -31,6 +18,3 @@
 </div>
 
 <script src="js/Forum-js.js"></script>
-
-</body>
-</html>
